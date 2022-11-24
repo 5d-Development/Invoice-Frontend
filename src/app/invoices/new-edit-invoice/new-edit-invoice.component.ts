9c6fc9df1631cb1e32d72923bd1a796d3e13ee6b
mohamed saber <mohamed.saber@5d-agency.com>
2022-11-24 15:07:04 +0200

    form

--- src/app/invoices/new-edit-invoice/new-edit-invoice.component.ts
+++ src/app/invoices/new-edit-invoice/new-edit-invoice.component.ts
@@ -3,7 +3,10 @@ import { Subscription } from 'rxjs';
 import { ActivatedRoute, Data } from '@angular/router';
 import { ActiveTabService } from 'src/app/shared/active-tab.service';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { MatDatepickerModule } from '@angular/material/datepicker';
+import { NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-edit-invoice',
@@ -13,6 +16,8 @@ import { MatDatepickerModule } from '@angular/material/datepicker';
 export class NewEditInvoiceComponent implements OnInit {
   activeTab: string = '';
   activeIcon: string = '../../assets/img/invoice.svg';
+  model1: NgbDateStruct;
+  model2: NgbDateStruct;
   activeTitleSubscribtion: Subscription = new Subscription;
   constructor(private route: ActivatedRoute, private activeTabService: ActiveTabService) { }
   // Form
@@ -22,7 +27,6 @@ export class NewEditInvoiceComponent implements OnInit {
 
   submit_form() {
     console.log('invoice form submitted successfully');
-
   }
 
   ngOnInit(): void {
